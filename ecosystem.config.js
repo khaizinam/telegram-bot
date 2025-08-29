49795f35068b5b48256a362f398e83627c720b4e
khai.nh <khai.nh@gmail.com>
2025-08-29 16:52:46 +0700

    save

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,5 +20,15 @@ module.exports = {
       error_file: "./logs/pm2/queue-default-error.log",
       log_date_format: "YYYY-MM-DD HH:mm:ss",
     },
+    {
+      name: "get_coin-job",
+      script: "jobs/get_coin.js",
+      interpreter: "node",
+      watch: false,
+      autorestart: true,
+      out_file: "./logs/pm2/get_coin-job-out.log",
+      error_file: "./logs/pm2/get_coin-job-error.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+    },
   ],
 };
