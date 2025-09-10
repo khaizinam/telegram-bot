c9b01a0351a31941bb8f2cdc57029c4c50194c71
khai.nh <khai.nh@gmail.com>
2025-09-10 08:57:13 +0700

    save

--- jobs/get_coin.js
+++ jobs/get_coin.js
@@ -53,7 +53,16 @@ async function processCoin(coinid, index) {
 
     const txt = `⚡ *${coinid} - ${formatPrice(newData.currentPrice)}*\n` +
                 `- ${trend} *${diff.toFixed(2)}%*\n` +
-                `- Thời gian: *${new Date().toLocaleString("vi-VN")}*\n` +
+                `- Thời gian: *${new Date().toLocaleString("vi-VN", {
+                    timeZone: "Asia/Ho_Chi_Minh",
+                    hour12: false,
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    second: "2-digit"
+                  })}*\n` +
                 `- Cao nhất 24h: *${formatPrice(newData.high24h)}*\n` +
                 `- Thấp nhất 24h: *${formatPrice(newData.low24h)}*`;
 
